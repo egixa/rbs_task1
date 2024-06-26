4dcc5f3a0bd203562d3da4fceba7a8bcfbe754b9
egixa <irona114@yandex.ru>
2024-06-26 15:42:59 +0400

    Правки первого задания grabber

--- task1.go
+++ task1.go
@@ -8,63 +8,92 @@ import (
 	"net/http"
 	"os"
 	"strings"
+	"time"
 )
 
+// Функция проверки валидности url и возврат тела сайта
+func getContent(url string) ([]byte, error) {
+
+	// Отправить GET-запрос
+	resp, err := http.Get(url)
+	if err != nil {
+		fmt.Println(time.Now().Format("01-02-2006 15:04:05"), "Ошибка запроса:", err)
+		return nil, err
+	}
+
+	//Проверить запрос
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf(time.Now().Format("01-02-2006 15:04:05"), "Неудачный ответ:", resp.Status)
+	}
+	defer resp.Body.Close()
+
+	// Прочитать содержимое ответа
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, fmt.Errorf(time.Now().Format("01-02-2006 15:04:05"), "Ошибка чтения ответа:", err)
+	}
+	return body, nil
+}
+
+// Запись тела сайта в новый файл по указанному пути
+func writeBody(content []byte, dstFolder *string, url string) {
+	domen := strings.Split(url, "/")
+
+	txtfile, err := os.Create(fmt.Sprintf("%s/%s.txt", *dstFolder, domen[len(domen)-2]))
+	if err != nil {
+		fmt.Println(time.Now().Format("01-02-2006 15:04:05"), "Ошибка при создании файла:", err)
+		return
+	}
+
+	_, err = txtfile.Write(content)
+	if err != nil {
+		fmt.Println(time.Now().Format("01-02-2006 15:04:05"), "Ошибка записи в файл:", err)
+		return
+	}
+	defer txtfile.Close()
+
+	fmt.Println(time.Now().Format("01-02-2006 15:04:05"), fmt.Sprintf("Запись страницы %s завершена", domen[len(domen)-2]))
+	return
+}
+
 func main() {
-	srcUrl := flag.String("src", "", "a src")
-	dstFolder := flag.String("dst", "", "a dst")
+
+	srcUrl := flag.String("src", "", "Путь до текстового файла, содержащий ссылки")
+	dstFolder := flag.String("dst", "", "Путь до папки для создания нового текстового файла")
 
 	flag.Parse()
+	if *srcUrl == "" || *dstFolder == "" {
+		fmt.Println(time.Now().Format("01-02-2006 15:04:05"), "Отсутствуют данные о местоположении файла и директории.")
+		fmt.Println("Ожидаемые данные:")
+		flag.PrintDefaults()
+
+		if *dstFolder == "" {
+			os.Mkdir(*dstFolder, 777)
+		}
+		return
+	}
 
 	// Открыть файл с URL-адресами
 	file, err := os.Open(*srcUrl)
 	if err != nil {
-		fmt.Println("Ошибка открытия файла:", err)
+		fmt.Println(time.Now().Format("01-02-2006 15:04:05"), "Ошибка открытия файла:", err)
 		return
 	}
+
 	defer file.Close()
+
 	// Читать строки из файла
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		url := scanner.Text()
 
-		// Отправить GET-запрос
-		resp, err := http.Get(url)
+		content, err := getContent(url)
+
 		if err != nil {
-			fmt.Println("Ошибка запроса:", err)
-			continue
-		}
-		defer resp.Body.Close()
-
-		if resp.StatusCode != http.StatusOK {
-			fmt.Println("Неудачный ответ:", resp.Status)
 			continue
 		}
 
-		// Прочитать содержимое ответа
-		body, err := io.ReadAll(resp.Body)
-		if err != nil {
-			fmt.Println("Ошибка чтения ответа:", err)
-			continue
-		}
-
-		// Записать содержимое ответа в файл
-		file, err = os.Create(fmt.Sprintf("%s/%s.txt", *dstFolder, strings.Split(resp.TLS.ServerName, ".")))
-		if err != nil {
-			fmt.Println("Ошибка создания файла:", err)
-			continue
-		}
-
-		_, err = file.Write(body)
-		if err != nil {
-			fmt.Println("Ошибка записи в файл:", err)
-			continue
-		}
-
-		if err := scanner.Err(); err != nil {
-			fmt.Println("Ошибка сканирования:", err)
-		}
-
-		fmt.Println("Обработка завершена.")
+		writeBody(content, dstFolder, url)
 	}
+	fmt.Println("цикл завершен")
 }
